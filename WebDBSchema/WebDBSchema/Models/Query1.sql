4f97498426f4ed2136c794cb2865c7302981b09d
Abel S. Salvatierra <assalvatierra@gmail.com>
2021-08-27 09:49:35 +0800

    templateProject

--- WebDBSchema/WebDBSchema/Models/Query1.sql
+++ WebDBSchema/WebDBSchema/Models/Query1.sql
@@ -1,1 +1,3 @@
-﻿select * from InvSuppliers;
+﻿insert into InvStores("StoreName") values ('Cebu Branch 1'),('Davao Main'),('Gensan Branch 1');
+
+select * from InvStores;
---
+++ WebInv/Server/Controllers/InvStoresController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDBSchema.Models;
+using WebInv.Server.Data;
+
+namespace WebInv.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvStoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvStoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/InvStores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InvStore>>> GetInvStore()
+        {
+            return await _context.InvStores.ToListAsync();
+        }
+
+        // GET: api/InvStores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<InvStore>> GetInvStore(int id)
+        {
+            var invStore = await _context.InvStores.FindAsync(id);
+
+            if (invStore == null)
+            {
+                return NotFound();
+            }
+
+            return invStore;
+        }
+
+        // PUT: api/InvStores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInvStore(int id, InvStore invStore)
+        {
+            if (id != invStore.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(invStore).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InvStoreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/InvStores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<InvStore>> PostInvStore(InvStore invStore)
+        {
+            _context.InvStores.Add(invStore);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInvStore", new { id = invStore.Id }, invStore);
+        }
+
+        // DELETE: api/InvStores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInvStore(int id)
+        {
+            var invStore = await _context.InvStores.FindAsync(id);
+            if (invStore == null)
+            {
+                return NotFound();
+            }
+
+            _context.InvStores.Remove(invStore);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InvStoreExists(int id)
+        {
+            return _context.InvStores.Any(e => e.Id == id);
+        }
+    }
+}
